7620f726f6b9ec6d81614593196b4402b58c16de
Iknowthings <Iknowthings0001@gmail.com>
2021-06-21 12:44:16 -0500

    Loop for text

--- MemGen.py
+++ MemGen.py
@@ -11,10 +11,10 @@ memeresult=path.join(memefolder, outputname)
 text="I will not instigate revolution."
 textsize=40
 origen=(50, 40)
+rep=16
 
 #Loads font and defines size
 comicFont = ImageFont.truetype('/usr/share/fonts/opentype/comic-neue/ComicNeue_Bold.otf', textsize)
-
 
 #Opens meme image
 meme = Image.open(memeimage).convert('RGB')
@@ -22,9 +22,9 @@ canvas = ImageDraw.Draw(meme)
 
 #write Text replace TODO with loop
 
-#TODO
-canvas.text(origen, text, font=comicFont, fill ="white")
-#TODO
+for  i in range(rep):
+    canvas.text((origen[0],origen[1]+40*i), text, font=comicFont, fill ="white")
+
 
 #Show image and save it
 meme.show()
